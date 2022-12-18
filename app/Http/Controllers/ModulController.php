0f36b519782efbfebae20aaaf093c4a843ef9c9d
Nopalev <faadhilah56@gmail.com>
2022-12-18 21:14:15 +0700

    added: basic controller index function

--- app/Http/Controllers/ModulController.php
+++ app/Http/Controllers/ModulController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Administrator;
+use App\Models\Dosen;
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ModulController extends Controller
 {
-    //
+    function index(){
+        $user = Mahasiswa::where('user_id', Auth::id())->first();
+        if($user){
+            return view('mhs.modul');
+        }
+        $user = Dosen::where('user_id', Auth::id())->first();
+        if($user){
+            return view('dosen.modul');
+        }
+        $user = Administrator::where('user_id', Auth::id())->first();
+        if($user){
+            return view('admin.modul');
+        }
+    }
 }
--- app/Http/Controllers/TugasController.php
+++ app/Http/Controllers/TugasController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Administrator;
+use App\Models\Dosen;
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TugasController extends Controller
 {
-    //
+    function index(){
+        $user = Mahasiswa::where('user_id', Auth::id())->first();
+        if($user){
+            return view('mhs.tugas');
+        }
+        $user = Dosen::where('user_id', Auth::id())->first();
+        if($user){
+            return view('dosen.tugas');
+        }
+        $user = Administrator::where('user_id', Auth::id())->first();
+        if($user){
+            return view('admin.tugas');
+        }
+    }
 }
